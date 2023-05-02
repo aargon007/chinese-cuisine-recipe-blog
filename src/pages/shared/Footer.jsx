96b54b5eb377f7573aa54263a6b48a1221e9904f
md muhaiminul <muhai4959@gmail.com>
2023-05-02 20:56:01 +0600

    add footer section

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import image from '../../assets/3.png'
+import { FaFacebook, FaInstagram, FaLinkedin, FaPinterest, FaTiktok, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='p-5 space-y-5 text-white bg-[url(https://www.tugagency.com/wp-content/uploads/2019/09/footer_background-1024x375.jpg)] bg-opacity-100 bg-no-repeat bg-cover bg-top'>
+            <h1 className="flex items-center justify-center font-bold gap-3">
+                    <span className='text-red-500 text-3xl'> 米隆 </span>
+                    <span className='font-kalam'>Milong <br /> <small className='font-sans'>Chinese Cuisine</small> </span>
+            </h1>
+            <div className='flex flex-col md:flex-row text-center gap-8'>
+                <div className='w-full'>
+                    <h1 className='text-xl font-semibold mb-2 '>Our Location</h1>
+                    <p>Hunan Province (southern central China) <br /> Changsha, Zhangjiajie</p>
+                </div>
+                <div className='w-full space-y-5'>
+                    <h1 className='text-xl font-semibold'>Get News and Offers</h1>
+                    <form className=''>
+                        <input type="email" name="email" id=""  className='bg-slate-200 px-2 outline-none text-black'/>
+                        <button className='bg-amber-500'>Subscribe</button>
+                    </form>
+                    <div className='flex justify-center gap-5'>
+                        <FaFacebook/>
+                        <FaTwitter/>
+                        <FaInstagram/>
+                        <FaPinterest/>
+                        <FaLinkedin/>
+                        <FaTiktok/>
+                    </div>
+                </div>
+                <div className='w-full space-y-3 text-center'>
+                    <h1 className='text-xl font-semibold '>Contact Us</h1>
+                    <p>800-837-9017</p>
+                    <p>800-837-9018</p>
+                    <p>milon@milongchinesecuisine.com</p>
+                </div>
+            </div>
+            <div className= ' space-x-4 text-amber-700 text-xl text-center'>
+                <span className=''>Home</span>
+                <span>Blog</span>
+                <span>Login</span>
+                <span>Recipes</span>
+            </div>
+            <p className='text-center'>Copyright © 2023 Milong. All Rights Reserved.</p>
         </div>
     );
 };
