5561162ad624c0c7286b6c36cf9ef00d2029330d
md muhaiminul <muhai4959@gmail.com>
2023-05-02 23:48:39 +0600

    add chef details page

--- src/pages/Home/Chef.jsx
+++ src/pages/Home/Chef.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const Chef = ({chef}) => {
+
     const {experience, id, name, num_recipes, picture, rating} = chef;
+
+    const navigate = useNavigate();
+    const handleNavigation = () => {
+        navigate(`/chef/${id}`)
+    }
+
     return (
         <div className='flex gap-3 bg-amber-200 rounded-xl '>
             <div className='w-7/12 p-5 leading-loose space-y-5'>
@@ -10,7 +17,7 @@ const Chef = ({chef}) => {
                 <p className='text-lg'>Years of Experience : {experience}</p>
                 <p className='text-lg'>Number of Recipes : {num_recipes}</p>
                 <p className='text-lg'>Rating : {rating}</p>
-                <button className='bg-amber-600 px-3 py-2 hover:bg-amber-800 transition-all rounded-xl border-none text-white'>View Recipes</button>
+                <button onClick={handleNavigation} className='bg-amber-600 px-3 py-2 hover:bg-amber-800 transition-all rounded-xl border-none text-white'>View Recipes</button>
             </div>
             <div className='w-5/12'>
                 <img src={picture} alt=""  className='w-full h-80 object-cover rounded-r-xl'/>
--- src/pages/chef/Details.jsx
+++ src/pages/chef/Details.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Recipe from './Recipe';
 
 const Details = () => {
+
+    const chefDetails = useLoaderData();
+    const {experience, id, name, num_recipes, picture, rating, description, recipes} = chefDetails;
+
+    console.log(chefDetails);
+
     return (
-        <div>
-            
+        <div className='py-10'>
+            <div className='w-8/12 mx-auto space-y-5 border bg-yellow-100 p-5 rounded-lg'>
+                <img src={picture} alt="" className='w-[100px] h-[100px] rounded-full mx-auto'/>
+                <h1 className='text-center text-2xl font-semibold'>{name}</h1>
+                <div className='flex gap-10 text-center justify-center text-xl'>
+                    <h1>
+                        {num_recipes} <br />
+                        <small className='text-red-500'>Recipe</small>
+                    </h1>
+                    <h1>
+                        {experience}y <br />
+                        <small className='text-red-500'>Experience</small>
+                    </h1>
+                    <h1>
+                        {rating} <br />
+                        <small className='text-red-500'>Rating</small>
+                    </h1>
+                </div>
+                <p className='leading-loose'>{description}</p>
+            </div>
+            <h1 className='text-3xl text-center font-semibold font-lobster my-10'>Popular Recipe</h1>
+            <div>
+                {
+                    recipes.map(recipe => <Recipe key={recipe.rating} recipe={recipe} />)
+                }
+            </div>
         </div>
     );
 };
