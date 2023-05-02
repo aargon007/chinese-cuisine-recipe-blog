92a2e9f71ef8a2769a9a23dbe90fe0cf2d4edb59
md muhaiminul <muhai4959@gmail.com>
2023-05-03 01:03:15 +0600

    add chef recipe details section

--- src/pages/chef/Details.jsx
+++ src/pages/chef/Details.jsx
@@ -10,9 +10,9 @@ const Details = () => {
     console.log(chefDetails);
 
     return (
-        <div className='py-10'>
-            <div className='w-8/12 mx-auto space-y-5 border bg-yellow-100 p-5 rounded-lg'>
-                <img src={picture} alt="" className='w-[100px] h-[100px] rounded-full mx-auto'/>
+        <div className='py-10 px-5 md:px-28'>
+            <div className='md:px-28 space-y-5 border bg-yellow-100 p-5 rounded-lg'>
+                <img src={picture} alt="" className='w-[150px] h-[150px] rounded-full mx-auto object-cover'/>
                 <h1 className='text-center text-2xl font-semibold'>{name}</h1>
                 <div className='flex gap-10 text-center justify-center text-xl'>
                     <h1>
@@ -31,7 +31,7 @@ const Details = () => {
                 <p className='leading-loose'>{description}</p>
             </div>
             <h1 className='text-3xl text-center font-semibold font-lobster my-10'>Popular Recipe</h1>
-            <div>
+            <div className='grid md:grid-cols-3 gap-10'>
                 {
                     recipes.map(recipe => <Recipe key={recipe.rating} recipe={recipe} />)
                 }
