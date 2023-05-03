90f4a4907abb757166f5bf96810d8d6e49480f0b
md muhaiminul <muhai4959@gmail.com>
2023-05-03 10:29:44 +0600

    create errorPage

--- src/pages/shared/ErrorElement.jsx
+++ src/pages/shared/ErrorElement.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useRouteError } from 'react-router-dom';
 
 const ErrorElement = () => {
+
+    const { error, status } = useRouteError()
+
     return (
-        <div>
-            
+        <div className='flex flex-col items-center w-screen h-screen'>
+            <img src="/3793096.jpg" alt="" className='w-96 mx-auto' />
+            <h1 className='text-3xl font-mono uppercase text-center text-red-700 font-semibold px-5 mb-10'>the page you were looking for does not exist</h1>
+            <button className='bg-blue-600 text-white py-3 px-5 font-mono border-none rounded-lg text-2xl'>Go back to Home</button>
         </div>
     );
 };
